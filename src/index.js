32827743d532991ef581f1e3ab5dd1e4aff34baf
Sirapavee <sirapavee@gmail.com>
2023-04-25 22:17:16 +0700

    refactor: update code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const dotenv = require('dotenv')
 const app = express()
 const env = dotenv.config().parsed
 
+// Create a router to handle routes
+const router = express.Router();
+
 const lineConfig = {
     channelAccessToken: env.ACCESS_TOKEN,
     channelSecret: env.SECRET_TOKEN
@@ -22,6 +25,16 @@ const handleEvent = async (e) => {
         return client.replyMessage(e.replyToken, { type: 'text', text: 'test' })
     }
 }
+
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
+  res.json({
+    hello: "hi!"
+  });
+});
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use(`/.netlify/functions/api`, router);
 
 app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
     try {
