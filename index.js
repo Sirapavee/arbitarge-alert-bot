240c7441022f061c112c81a40f54026365770298
Sirapavee <sirapavee@gmail.com>
2023-04-25 21:49:33 +0700

    feat: line bot

---
+++ index.js
@@ -0,0 +1,39 @@
+const line = require('@line/bot-sdk')
+const express = require("express")
+const axios = require('axios').default
+const dotenv = require('dotenv')
+
+const app = express()
+const env = dotenv.config().parsed
+
+const lineConfig = {
+    channelAccessToken: process.env.ACCESS_TOKEN,
+    channelSecret: process.env.SECRET_TOKEN
+}
+
+const client = new line.Client(lineConfig)
+
+const handleEvent = async (e) => {
+    console.log(e);
+    if (e.type !== 'message' || e.message.type !== text) {
+        return null
+    }
+    else if (e.type === 'message') {
+        return client.replyMessage(e.replyToken, { type: 'text', text: 'test' })
+    }
+}
+
+app.post('/webhook', line.middleware(lineConfig), async (req, res) => {
+    try {
+        const events = req.body.events
+        console.log('event: ', events);
+        return events.length > 0 ? await events.map((item) => handleEvent(item)) : res.status(200).send('OK')
+    } catch (e) {
+        res.status(500).end()
+    }
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
